092364bd7e6a532bed5db9592844535eb3f64d76
Easiniur Rashid Prantick <34379614+Prantick40th@users.noreply.github.com>
2022-10-29 23:16:26 +0600

    Add files via upload

---
+++ JavaApplication13/src/runProject.java
@@ -0,0 +1,10 @@
+
+public class runProject {
+    public static void main(String[] args) {
+        
+        Login ob = new Login();
+        
+        ob.setVisible(true);
+        //ob1.setVisible(true);
+    }
+}
